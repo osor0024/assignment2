31e79f2e7e712a028c4515e4fe8c562db65f96f1
osor0024 <osor0024@algonquinlive.com>
2018-10-24 19:10:59 -0400

    first try

---
+++ namespace.js
@@ -0,0 +1,20 @@
+let b = null;
+var OSOR0024 = {
+    init: function () {
+    document.createElement("div");
+        d.className.add("box");
+        b = document.querySelector(".box");
+        b.textContent("osor0024");
+        document.getElementById("boxes").appendChild("div");
+
+
+/********************************
+     Events
+*********************************/
+
+        b.addEventListener("mouseover", mouseOver);
+        b.addEventListener("mouseoute", mouseOut);
+        b.addEventListener("click", click);
+        
+    }
+}
