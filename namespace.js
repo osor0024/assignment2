6c4a415ce8e10489b42b1ac1cd02f7e96fb729d7
osor0024 <osor0024@algonquinlive.com>
2018-10-25 16:51:40 -0400

    17

--- namespace.js
+++ namespace.js
@@ -1,9 +1,11 @@
 var OSOR0024 = {
+    
     init: function () {
+        
         let newDiv = document.createElement("div");
-        newDiv.className= ("box");
-        newDiv.textContent= ("osor0024");
-        document.getElementById("boxes").appendChild("newDiv");
+        newDiv.className = ("box");
+        newDiv.textContent = ("osor0024");
+        document.getElementById("boxes").appendChild(newDiv);
 
 
         newDiv.addEventListener("mouseover", function () {
@@ -21,25 +23,3 @@ var OSOR0024 = {
         });
     }
 }
-        
-    //    },
-
-    //    mouseover: function (e) {
-    //        e.target.classList.toggle("highlight");
-    //        newDiv.addEventListener("mouseover", mouseOver);
-    //    },
-    //
-    //    mouseout: function (e) {
-    //        e.target.classList.toggle("highlight");
-    //        e.target.removeAttribute("highlight");
-    //        newDiv.addEventListener("mouseout", mouseOut);
-    //    },
-    //
-    //    click: function () {
-    //        this.style.borderColor = "pink";
-    //        this.style.backgroundColor = "red";
-    //        console.log(this);
-    //        newDiv.addEventListener("click", click);
-    //    }
-
-
