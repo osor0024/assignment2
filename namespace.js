3a164b7407eed84a786bd192faee115fbc1ccebe
osor0024 <osor0024@algonquinlive.com>
2018-10-25 09:11:52 -0400

    10

--- namespace.js
+++ namespace.js
@@ -4,26 +4,43 @@ var OSOR0024 = {
         newDiv.className("box");
         newDiv.textContent("osor0024");
         document.getElementById("boxes").appendChild("div");
-    },
 
-    mouseover: function (e) {
-        e.target.classList.toggle("highlight");
-        newDiv.addEventListener("mouseover", mouseOver);
-    },
 
-    mouseout: function (e) {
-        e.target.classList.toggle("highlight");
-        e.target.removeAttribute("highlight");
-        newDiv.addEventListener("mouseout", mouseOut);
-    },
+        newDiv.addEventListener("mouseover", function () {
+            newDiv.classList.toggle("highlight");
+        });
 
-    click: function () {
-        this.style.borderColor = "pink";
-        this.style.backgroundColor = "red";
-        console.log(this);
-        newDiv.addEventListener("click", click);
+        newDiv.addEventListener("mouseout", function () {
+            newDiv.classList.toggle("highlight");
+            newDiv.removeAttribute("style");
+        });
+
+        newDiv.addEventListener("click", function () {
+            newDiv.style.borderColor = "pink";
+            newDiv.style.backgroundColor = "red";
+        });
     }
+    
+    
+    //    },
+
+    //    mouseover: function (e) {
+    //        e.target.classList.toggle("highlight");
+    //        newDiv.addEventListener("mouseover", mouseOver);
+    //    },
+    //
+    //    mouseout: function (e) {
+    //        e.target.classList.toggle("highlight");
+    //        e.target.removeAttribute("highlight");
+    //        newDiv.addEventListener("mouseout", mouseOut);
+    //    },
+    //
+    //    click: function () {
+    //        this.style.borderColor = "pink";
+    //        this.style.backgroundColor = "red";
+    //        console.log(this);
+    //        newDiv.addEventListener("click", click);
+    //    }
 
 
-}
-OSOR0024.init()
+};
