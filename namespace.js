3f25836d9e0e276f9965143f7eac3d939624a7f6
osor0024 <osor0024@algonquinlive.com>
2018-10-25 17:04:05 -0400

    last one

--- namespace.js
+++ namespace.js
@@ -20,6 +20,7 @@ var OSOR0024 = {
         newDiv.addEventListener("click", function () {
             newDiv.style.borderColor = "pink";
             newDiv.style.backgroundColor = "red";
+            newDiv.textContent("Hola!!");
         });
     }
 }
